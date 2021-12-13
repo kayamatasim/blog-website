8d8c8e05ccb2b06150be64a2b0b9e4553ec46947
asimsheikh@lidolearning.com <asimsheikh@lidolearning.com>
2021-12-13 19:57:13 +0530

    final working form

--- script.js
+++ script.js
@@ -1,15 +1,68 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+
+let submit=document.querySelector('.submit');
+
+
+
+
+submit.addEventListener('click',()=>{
+  let title=document.querySelector('#title').value;
+  let content=document.querySelector('#content').value;
+  let mainc=document.querySelector('#mainc').value;
+ 
+ if(localStorage.getItem('data')==null){
+   console.log('noting here')
+  let info=[{title:title,content:content,mainc:mainc}]
+   localStorage.setItem('data',JSON.stringify(info));
+ }
+
+ else{
+   let info=JSON.parse(localStorage.getItem('data'));
+   info.push({title:title,content:content,mainc:mainc})
+   console.log(info);
+   localStorage.setItem('data',JSON.stringify(info));
+ }
+})
+
+// setInterval(asim,1000)
+asim()
+    function asim(){
+
+
+      if(localStorage.getItem('data')==null){
+        console.log('noting here')
+        document.querySelector('.blogs').innerHTML=`<h1>no blogs to show right now</h1>`
+      }
+     
+      else{
+        let info=JSON.parse(localStorage.getItem('data'));
+info.forEach((i,index)=>{
+  console.log();
   
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
+  document.querySelector('.blogs').innerHTML+=` <div class="blog">
+  <h1 class="blog-heading purple">${i.title}</h1>
+  <p class="blog-text">
+     <span>${i.mainc}</span>
+     <span class="dots">...</span>       
+      <span class="more collapse" id="collapse${index}"> ${i.content} </span>
+    
+  </p>
+  <button class="btn btn-sm color myBtn" data-bs-toggle="collapse" href="#collapse${index}">read</button>
+</div>`
+})
+      }
+
+
+
+
+   
     }
-  }
+    
+   let fog= document.querySelector('.fog');
+   fog.addEventListener('click',()=>{
+     localStorage.clear()
+     console.log(localStorage.getItem('data'))
+     console.log('clicked')
+     
+   })
+
+  
